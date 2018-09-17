f5467e9b468f652cfcb8ee88a5a32f60f54fb109
leolulu <348699103@qq.com>
2018-09-17 20:31:24 +0800

    yandere2

--- 爬虫/yandere机动下载原图2.py
+++ 爬虫/yandere机动下载原图2.py
@@ -6,21 +6,22 @@ page_counter = 0
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36"
 }
-base_url = 'https://yande.re/post?page={}'
+base_url = 'https://yande.re/post?page={}&tags=sakimichan'
 
 session = requests.session()
 
-while page_counter < 200:
+while page_counter < 10:
     r = session.get(base_url.format(page_counter), headers=headers)
 
-    for i in re.findall('https://files.yande.re/jpeg.*?.jpg', r.content.decode()):
-        print('url: ',i)
-        pic_name = re.findall('yande.re%(.*?).jpg',i)[-1].replace('%20','_')
-        print('pic_name: ',pic_name)
+    for i in set(re.findall(r'https://files.yande.re/[jpeg|image].*?\.(?:jpg|png)', r.content.decode())):
+        print('url: ', i)
+        pic_name = re.findall(r'yande.re%(.*?)\.(?:jpg|png)',
+                              i)[-1].replace('%20', '_')
+        print('pic_name: ', pic_name)
         img = session.get(i, headers=headers)
         with open('./public/gelbooru/{}.jpg'.format(pic_name), 'wb') as f:
             f.write(img.content)
-        print('pic_num: ',pic_num)
+        print('pic_num: ', pic_num)
         pic_num += 1
 
     page_counter += 1
