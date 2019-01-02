b408a62c6b0dc7a1e0ee1526ae9fb433f2825c8e
leolulu <348699103@qq.com>
2019-01-02 12:04:20 +0800

    Update 新yandere-多线程.py

--- 爬虫/新yandere-多线程.py
+++ 爬虫/新yandere-多线程.py
@@ -1,8 +1,10 @@
 import requests
 from lxml import etree
 import time
+import os
+from retrying import retry
+from concurrent.futures import ThreadPoolExecutor
 
-page_num = 1
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 }
@@ -10,19 +12,60 @@ proxies = {
     "http": "socks5://127.0.0.1:1080",
     'https': 'socks5://127.0.0.1:1080'
 }
-next_url = 'https://yande.re/post?tags=girls_frontline'
 
-while next_url is not None:
-    r = requests.get(next_url,proxies=proxies).content
-    img_urls = etree.HTML(r).xpath("//ul[@id='post-list-posts']/li/a/@href")
-    next_url = 'https://yande.re' + etree.HTML(r).xpath("//a[@class='next_page']/@href")[0] if len(etree.HTML(r).xpath("//a[@class='next_page']/@href")) > 0 else None
-    for img in img_urls:
-        print('downloading: ','page:'+str(page_num),img)
-        with open("./public/yandere/"+img.split('/')[-1].replace('%20','_').replace('yande.re_',''),'wb') as f:
-            try:
-                f.write(requests.get(img,timeout=600).content)
-            except Exception as e:
-                print(e)
-                time.sleep(60)
-    page_num += 1
-    
+try:
+    os.makedirs('./public/yandere/')
+except:
+    pass
+
+# next_url_list = [
+#     'https://yande.re/post?tags=fishnets',
+#     'https://yande.re/post?tags=garter',
+#     'https://yande.re/post?tags=heels',
+#     'https://yande.re/post?tags=pantyhose'
+# ]
+
+next_url_list = [
+    'https://yande.re/post?tags=armor+torn_clothes'
+]
+pic_url_list = []
+length_of_pic_left = 0
+
+
+@retry(wait_fixed=5000, stop_max_attempt_number=7)
+def processing(next_url):
+    page_num = 1
+    # next_url = 'https://yande.re/post?tags=g41_%28girls_frontline%29'
+    while next_url is not None:
+        r = requests.get(next_url, proxies=proxies, headers=headers).content
+        img_urls = etree.HTML(r).xpath("//ul[@id='post-list-posts']/li/a/@href")
+        pic_url_list.extend(img_urls)
+        next_url = 'https://yande.re' + etree.HTML(r).xpath("//a[@class='next_page']/@href")[0] if len(etree.HTML(r).xpath("//a[@class='next_page']/@href")) > 0 else None
+        print('current Page.{},length of pic list is {}.'.format(page_num, len(pic_url_list)))
+        page_num += 1
+
+# @retry(wait_fixed=5000, stop_max_attempt_number=7)
+def downloadPic(img_url):
+    global length_of_pic_left
+    # print('downloading: ', 'page:{} '.format(str(page_num)), img_url)
+    with open("./public/yandere/"+img_url.split('/')[-1].replace('%20', '_').replace('yande.re_', ''), 'wb') as f:
+        try:
+            f.write(requests.get(img_url, timeout=600, proxies=proxies, headers=headers).content)
+            length_of_pic_left -= 1
+            print('{}pics left to download.'.format(length_of_pic_left))
+        except Exception as e:
+            print(e)
+            time.sleep(60)
+        # f.write(requests.get(img_url, timeout=600, proxies=proxies, headers=headers).content)
+        # length_of_pic_left -= 1
+        # print('{}pics left to download.'.format(length_of_pic_left))
+
+
+for url in next_url_list:
+    processing(url)
+print('total list crawl finish.')
+
+length_of_pic_left = len(pic_url_list)
+
+with ThreadPoolExecutor(max_workers=4) as excutor:
+    excutor.map(downloadPic, pic_url_list)
