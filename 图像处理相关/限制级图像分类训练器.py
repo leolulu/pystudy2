7f2763c32aa6a24c9135f7e8e1dee5faf29f684a
leolulu <348699103@qq.com>
2019-01-23 15:26:16 +0800

    Update 限制级图像分类训练器.py

--- 图像处理相关/限制级图像分类训练器.py
+++ 图像处理相关/限制级图像分类训练器.py
@@ -7,7 +7,7 @@ from sklearn.externals import joblib
 
 def timePrint(*args):
     import datetime
-    print('['+datetime.datetime.now().strftime('%F %X')+']', end='')
+    print('['+datetime.datetime.now().strftime('%F %X')+']', end=' ')
     for i in args[:-1]:
         print(i, end=' ')
     print(args[-1])
