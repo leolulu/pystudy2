e415bcfce25f23acd4422308e09ebd9587a30977
leolulu <35873316+leolulu@users.noreply.github.com>
2020-08-12 00:30:56 +0800

    Update konachan - 进入取每一个作者全部.py
    两年前的坑，终于填上了

--- 爬虫/konachan - 进入取每一个作者全部.py
+++ 爬虫/konachan - 进入取每一个作者全部.py
@@ -2,7 +2,8 @@ import requests
 from lxml import etree
 from urllib import parse
 import os
-
+from retrying import retry
+from concurrent.futures import ThreadPoolExecutor
 
 # while page_num < 10:
 #     r = session.get('http://konachan.com/post?page={}&tags='.format(page_num), headers=headers, proxies=proxies)
@@ -23,17 +24,17 @@ class KonachanInner:
     def __init__(self):
         self.except_artist_tag = 'tagme (artist)'
         self.page_num = 1
-        self.base_url = 'http://konachan.com/post'
+        self.base_url = 'http://konachan.net/post'
         self.headers = {
             "Cookie": "vote=1; __utmz=20658210.1460984814.55.2.utmcsr=konachan.com|utmccn=(referral)|utmcmd=referral|utmcct=/post/switch; __cfduid=d18af1d27bb5882a6eff521053cb3cc801525011444; tag-script=; country=US; blacklisted_tags=%5B%22%22%5D; konachan.net=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJTlhYTcwNjk4YzI4NjdjYWU2YjZhYzg2YTZiOWRlZmQ1BjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMTIxZXdwajVzYVRuR0huSWtWWEUrdlJPOE1EMUdCMHdUdG5yMjFXNmNVNm89BjsARg%3D%3D--8a1e71bdae987934cb60ab31c927084b3c5d85c6; __utmc=20658210; Hm_lvt_ba7c84ce230944c13900faeba642b2b4=1536069861,1536796970,1536939737,1537279770; __utma=20658210.97867196.1446035811.1537370253.1537509624.843; __utmt=1; forum_post_last_read_at=%222018-09-21T08%3A00%3A31%2B02%3A00%22; __utmb=20658210.3.10.1537509624; Hm_lpvt_ba7c84ce230944c13900faeba642b2b4=1537509630",
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
         }
         self.proxies = {
-            "http": "socks5://127.0.0.1:1080",
-            'https': 'socks5://127.0.0.1:1080'
+            "http": "socks5://127.0.0.1:10808",
+            'https': 'socks5://127.0.0.1:10808'
         }
         self.session = requests.session()
-        self.artist_exsist_list = []
+        self.artist_exsist_set = set()
 
     def extract(self, item):
         if len(item) == 0:
@@ -41,11 +42,14 @@ class KonachanInner:
         else:
             return item[0]
 
-    def new_folder(self, folder_name):
+    def new_folder(self, artist_folder_name):
         '''
         新建文件夹
         '''
-        os.makedirs('./public/konachan/{}'.format(folder_name))
+        folder_path = './public/konachan/{}'.format(artist_folder_name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        return folder_path
 
     def artist_exsist_func(self, artist_tag_name):
         """
@@ -53,12 +57,12 @@ class KonachanInner:
         包括方法调用
         返回要处理的作者名称
         """
-        if artist_tag_name in self.artist_exsist_list:
+        if artist_tag_name in self.artist_exsist_set:
             print(artist_tag_name, ': 此artist已存在，跳过')
         elif artist_tag_name == self.except_artist_tag:
             print(artist_tag_name, ': 系通用artist，跳过')
         else:
-            self.artist_exsist_list.append(artist_tag_name)
+            self.artist_exsist_set.add(artist_tag_name)
             return artist_tag_name  # 对于第一次遇到的作者调用处理方法
 
     def url_compliet(self, url):
@@ -77,14 +81,18 @@ class KonachanInner:
         r = self.session.get(url, headers=self.headers, proxies=self.proxies)
         return etree.HTML(r.content)
 
-    def parse_mainpage(self):
+    def parse_mainpage(self, page_capacity):
         """
         解析每一个首页
         """
-        html = self.parse_url('{}?page={}&tags='.format(self.base_url, self.page_num))
-        detail_page_urls = html.xpath("//ul[@id='post-list-posts']/li/div/a/@href")
-        detail_page_urls = map(self.url_compliet, detail_page_urls)
-        return detail_page_urls
+        detail_page_urls_list = []
+        for _ in range(page_capacity):
+            html = self.parse_url('{}?page={}&tags='.format(self.base_url, self.page_num))
+            detail_page_urls = html.xpath("//ul[@id='post-list-posts']/li/div/a/@href")
+            detail_page_urls = map(self.url_compliet, detail_page_urls)
+            detail_page_urls_list.extend(detail_page_urls)
+            self.page_num += 1
+        return detail_page_urls_list
 
     def parse_detail_page(self, url_list, deal_func):
         """
@@ -103,8 +111,13 @@ class KonachanInner:
                     next_url = self.url_compliet(self.extract(html.xpath("//a[text()='Next →']/@href")))
                     img_url_list = html.xpath("//ul[@id='post-list-posts']/li/a/@href")
                     # self.download_img(img_url_list)
-                    print(img_url_list)
-                # 2.获取图像
+                    folder_path = self.new_folder(artist_name)
+                    # 2.获取图像
+                    with ThreadPoolExecutor(max_workers=8) as executor:
+                        for img_url in img_url_list:
+                            executor.submit(self.downloadPic, img_url, folder_path, artist_name)
+                    # for img_url in img_url_list:
+                    #     self.downloadPic(img_url, folder_path)
 
     # def download_img(self, img_url_list):
     #     for i in img_url_list:
@@ -113,8 +126,20 @@ class KonachanInner:
     #         with open('./public/konachan/'+pic_name, 'wb') as f:
     #             f.write(session.get(i, proxies=proxies).content)
 
+    @retry(wait_exponential_multiplier=1000, wait_exponential_max=60000)
+    def downloadPic(self, img_url, folder_path, artist_name):
+        pic_path_name = os.path.join(folder_path, artist_name + img_url.split('/')[-1].replace('%20', '_').replace('Konachan.com_-_', ''))
+        print('begin：', img_url)
+        if os.path.exists(pic_path_name) == True:
+            os.remove(pic_path_name)
+        else:
+            content = requests.get(img_url, timeout=600, proxies=self.proxies, headers=self.headers).content
+            with open(pic_path_name, 'wb') as f:
+                f.write(content)
+        print('finish：', img_url)
+
     def run(self):
-        detail_page_urls = self.parse_mainpage()  # 解析首页，获得每一个详情页的地址
+        detail_page_urls = self.parse_mainpage(10)  # 解析首页，获得每一个详情页的地址
         self.parse_detail_page(detail_page_urls, self.artist_exsist_func)  # 解析详情页(包括回调函数)
 
 
