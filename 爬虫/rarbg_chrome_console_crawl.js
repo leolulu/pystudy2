97d890d376180d4afd932ed66145df75b802f993
leolulu <35873316+leolulu@users.noreply.github.com>
2020-08-14 15:25:19 +0800

    增加了自动翻页的功能

--- 爬虫/rarbg_chrome_console_crawl.js
+++ 爬虫/rarbg_chrome_console_crawl.js
@@ -4,6 +4,8 @@ for (i of document.getElementsByClassName("lista2")) {
         "https://rarbgprx.org" + i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("href")
     )
 }
+var a_list = document.getElementById("pager_links").getElementsByTagName("a")
+setTimeout("a_list[a_list.length-1].click()",1000)
 
 
 /*
